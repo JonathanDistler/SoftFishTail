12589512e35ceb53532e662404fccc346bdd2821
JonathanDistler <149265757+JonathanDistler@users.noreply.github.com>
2025-06-20 09:46:43 +0000

    adds a waittime

--- PiCam.py
+++ PiCam.py
@@ -11,15 +11,18 @@ picam2 = Picamera2()
 config = picam2.create_still_configuration(main={"size": (2592, 1944)})
 picam2.configure(config)
 
-# Start camera
+# Start camera once
 picam2.start()
 sleep(2)  # Let camera warm up
 
-# Capture image with timestamp
-shutter_number=3
-for i in range (shutter_number):
+# Take multiple pictures
+shutter_number = 3
+for i in range(shutter_number):
     time_val = str(datetime.now())
     image_path = f"{home_dir}/Desktop/max{i}.jpg"
     picam2.capture_file(image_path)
-
     print(f"Photo taken at {time_val} and saved to {image_path}")
+    sleep(.5)  #wait time
+
+# Optional: stop camera when done
+picam2.stop()
