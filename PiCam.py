4dd9142fc629aacf7fabf669537bba0df69ed43e
JonathanDistler <149265757+JonathanDistler@users.noreply.github.com>
2025-06-20 09:36:10 +0000

    updates

--- PiCam.py
+++ PiCam.py
@@ -1,23 +1,22 @@
-#import statements
-from picamera import Camera
+from picamera import PiCamera
 from time import sleep
 import os
 from datetime import datetime
 
-#saves to home directory
-home_dir=os.environ["HOME"]
-cam=Camera()
+# Saves to home directory
+home_dir = os.environ["HOME"]
+cam = PiCamera()
 
-#changes resolution
-cam.still_size=(2592,1944)
+# Changes resolution
+cam.resolution = (2592, 1944)
 cam.start_preview()
 
-#adds a timestamp and annotation
-time_val=str(datetime.now())
-cam.annotate("Hello. It's Jonathan Distler: {time_val}")
+# Adds a timestamp and annotation
+time_val = str(datetime.now())
+cam.annotate_text = f"Hello. It's Jonathan Distler: {time_val}"
 sleep(2)
 
-cam.take_photo(f"{home_dir}/Desktop/max.jpg")
+cam.capture(f"{home_dir}/Desktop/max.jpg")
 cam.stop_preview()
 
 #for better fps, auto exposure mode
