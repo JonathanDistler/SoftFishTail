20f405e77675c7208d76f99e0deb55b32368fd33
JonathanDistler <jhd94@cornell.edu>
2025-06-23 16:09:44 +0200

    transitions to launch not viewer

--- MujocoTest.py
+++ MujocoTest.py
@@ -7,30 +7,17 @@ model = mujoco.MjModel.from_xml_path(
 )
 data = mujoco.MjData(model)
 
-viewer = mujoco.viewer.launch_passive(model, data)
-
 joint_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, 'slider_joint')
 
-try:
-    while True:
-        dofadr = model.jnt_dofadr[joint_id]   # Starting DOF address of the joint
-        n_dof = 1   # Number of DOFs for this joint- hard coded as 1 
+def callback():
+    dofadr = model.joint_dofadr[joint_id]
+    n_dof = model.joint_dofnum[joint_id]
+    slider_force = data.qfrc_constraint[dofadr:dofadr + n_dof]
+    if n_dof == 1:
+        print(f"Force exerted by slider joint: {slider_force[0]:.4f} N")
+    else:
+        print(f"Force exerted by slider joint (all DOFs): {slider_force}")
+    mujoco.mj_step(model, data)
 
-        slider_force = data.qfrc_constraint[dofadr : dofadr + n_dof]
-
-        if n_dof == 1:
-            force_value = slider_force[0]
-            print(f"Force exerted by slider joint: {force_value:.4f} N")
-        else:
-            print(f"Force exerted by slider joint (all DOFs): {slider_force}")
-
-        mujoco.mj_step(model, data)
-        viewer.render()
-
-        time.sleep(0.01)
-
-except KeyboardInterrupt:
-    print("\nCtrl+C detected, closing viewer...")
-
-finally:
-    viewer.close()
+# Launch blocking viewer, passing callback to be called every frame
+mujoco.viewer.launch(model, data, callback)
