62b6255f7f570ea00d27f9339f24a4a2f37ba2f3
JonathanDistler <149265757+JonathanDistler@users.noreply.github.com>
2025-06-27 14:15:16 +0200

    Update Calibration.m
    changes out averaging algorithms for linear interpolation model

--- Calibration.m
+++ Calibration.m
@@ -2,56 +2,55 @@ clf()
 
 %uses script from https://learn.sparkfun.com/tutorials/load-cell-amplifier-hx711-breakout-hookup-guide to calibrate a necessary constant for the 
 %hx711 force tester
-%masses in kg
+
+%linear interpolation formula
+%y1+(y2-y1)/(x2-x1)*(x-x1)
+
+%masses in kg, the first number represents the measured mass
+%the other masses provide range for linear interpolation
 mass_1=.211;
-mass_2=.311;
-mass_3=.411;
+x2_1=.3;
+x1_1=.2;
 
 %ranges for calibration constants for the HX711 force sensor
 mass_1_range=[-325050,-1962050];
+
+%gives a linear interpolation to find the interpolated value for the
+%calibration value of the first mass 
+cal_1=mass_1_range(1)+((mass_1_range(2)-mass_1_range(1))/(x2_1-x1_1))*mass_1
+
+mass_2=.311;
+x2_2=.4;
+x1_2=.3;
 mass_2_range=[-2872050,-2052050];
+
+cal_2=mass_2_range(1)+((mass_2_range(2)-mass_2_range(1))/(x2_2-x1_2))*mass_2
+
+mass_3=.411;
+x2_3=.5;
+x1_3=.4;
 mass_3_range=[-2712050,-2122050];
+
+cal_3=mass_3_range(1)+((mass_3_range(2)-mass_3_range(1))/(x2_3-x1_3))*mass_3
+
+
 
 %creates basis for the the x and y axes
 masses=[mass_1,mass_2,mass_3];
-range_1=[mass_1_range(1),mass_2_range(1),mass_3_range(1)];
-range_2=[mass_1_range(2),mass_2_range(2),mass_3_range(2)];
 
-%mass-averaged difference between the upper and lower range
-mass_1_diff=abs(mass_1_range(1)-mass_1_range(2));
-mass_1_avg=mass_1_diff/mass_1;
+%calibrated array
+cals=[cal_1,cal_2,cal_3];
+
+%uses polyfit to fit the lines in all instances (1st degree polynomial)
+slope_fit = polyfit(masses,cals,1);
+slope_val=slope_fit(1)
 
 
-mass_2_diff=abs(mass_2_range(1)-mass_2_range(2));
-mass_2_avg=mass_2_diff/mass_2;
+hold on 
+plot(masses,cals, "LineWidth",2, "DisplayName","Mass vs. Calibration-Value")
+hold off
 
-mass_3_diff=abs(mass_3_range(1)-mass_3_range(2));
-mass_3_avg=mass_3_diff/mass_3;
+xlabel("Mass (kg)")
+ylabel("Calibration-Value (unitless)")
+title("Mass vs. Calibration-Value")
 
-%compiles averages into an array
-mass_avgs=[mass_1_avg,mass_2_avg,mass_3_avg];
-
-%uses polyfit to fit the lines in all instances
-slope_upper_bound = polyfit(masses,range_1,1);
-upper_slope=slope_upper_bound(1)
-slope_lower_bound = polyfit(masses,range_2,1);
-lower_slope=slope_lower_bound(1)
-slope_avg = polyfit(masses,mass_avgs,1) ;
-avg_slop=slope_avg(1)
-
-%just typical average value of the upper and lower bounded slopes for
-%calibration
-total_avg=(upper_slope+lower_slope)/2
-
-% %%%
-% hold on 
-% plot(masses,range_1, "LineWidth",2, "DisplayName","Mass vs. Upper-Bound")
-% plot(masses,range_2,"LineWidth",2,"DisplayName","Mass vs. Lower-Bound")
-% hold off
-% 
-% xlabel("Mass (kg)")
-% ylabel("Calibration-Value (unitless)")
-% title("Mass vs. Calibration-Value")
-% 
-% plot(masses,mass_avgs,"LineWidth",2,"DisplayName","Mass vs. Mass-Averaged Difference")
-% % %%%
